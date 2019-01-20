edafb78ff8cd75cc14c59bfd7b9a9684c3fe2d5a
James Woolfenden <James.Woolfenden@gmail.com>
2019-01-20 11:49:23 +0000

    uses registry

--- examples/exampleA/module.budget.tf
+++ examples/exampleA/module.budget.tf
@@ -1,5 +1,6 @@
 module "budget" {
-  source            = "github.com/jameswoolfenden/terraform-aws-budget"
+  source            = "JamesWoolfenden/budget/aws"
+  version           = "0.0.2"
   limit             = "${var.limit}"
   currency          = "${var.currency}"
   time_period_start = "${var.time_period_start}"
